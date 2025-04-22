fef64f067522dd679b70e41f8c38a708cb69e72d
Tarikul69 <tarikulabir69@gmail.com>
2025-04-23 01:41:47 +0600

    Update Database model

--- grofast/admin_panel/models.py
+++ grofast/admin_panel/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 # User Model
 class Users_table(models.Model):
+    users_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
@@ -29,4 +30,12 @@ class shop(models.Model):
     is_verified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     
-    
+ #review_table 
+class Review_table(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Users_table.users_id, on_delete=models.CASCADE)
+    shop = models.ForeignKey(shop, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return f"Rating: {self.rating}"
