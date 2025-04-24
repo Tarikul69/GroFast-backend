53a4beaa871390d4558588e9d801391848e064a0
Tarikul69 <tarikulabir69@gmail.com>
2025-04-24 23:38:56 +0600

    Update login registration api

--- grofast/admin_panel/serializers.py
+++ grofast/admin_panel/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Users_table, shop
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
 
 class UsersTableSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +17,18 @@ class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = shop
         fields = '__all__'
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
--- grofast/admin_panel/urls.py
+++ grofast/admin_panel/urls.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 
 from admin_panel import views
+from admin_panel.controller.login_registration_controller import LoginAPI, RegisterAPI
 from . import views
 
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
-    path('users/', views.users_list, name='users_list'),
+    path('users/', views.users_list, name='users'),
     path('shop_registration/', views.shop_registration, name='shop_registration'),
     path('shop_list/', views.shop_list, name='shop_list'),
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
 
     #Admin Panel UI path
     path('admin_index/', views.admin_index, name='admin_index'),
+    
 ]
